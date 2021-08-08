e647fb1a6e362af56f73c25d3b51493236ce0347
devaltai <yalatov@gmail.com>
2021-08-08 21:52:44 +0700

    client

--- src/app/bus/pet/petReturned.js
+++ src/app/bus/pet/petReturned.js
@@ -1,10 +1,25 @@
 //Core
 import React from "react"
 
+//Hooks
+import {usePedReturned} from "./hooks/usePetReturned"
+
 export const PetReturned = () => {
+  const {pet, error} = usePedReturned()
+  const petJSX = pet && (
+    <>
+      <p>Id: {pet.id}</p>
+      <p>Name: {pet.name}</p>
+    </>
+  )
+
+  const errorJSX = error && <p>We have a problem: {error.message}</p>
+
   return (
     <>
-      <h1>Pet return</h1>
+      <h1>Pet Returned</h1>
+      {petJSX}
+      {errorJSX}
     </>
   )
 }
